e22d876a976aa7681cceb67976ea3d782c811918
syuhada210 <syuhada@cytron.com.my>
2015-06-26 13:16:10 +0800

    Sample code of DIY PR19 using MPLAB and MPLABX

---
+++ PIC16F876A DIY PR29/MPLABX/PR29.X/build/default/production/_ext/1142487903/PR29.p1.d
@@ -0,0 +1,2 @@
+build/default/production/_ext/1142487903/PR29.p1:	\
+	../../Source\ file/uart_io.h	\
---
+++ PIC16F876A DIY PR29/MPLABX/PR29.X/build/default/production/_ext/1142487903/uart_io.p1.d
@@ -0,0 +1,2 @@
+build/default/production/_ext/1142487903/uart_io.p1:	\
+	../../Source\ file/uart_io.h	\
